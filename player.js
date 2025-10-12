e6b8d1109152df02fe6fa87afa7f070cb3f41bdc
Nagiishim <Dikeogbogu@gmail.com>
2025-10-12 23:10:26 +0100

    Update player.js

---
+++ player.js
@@ -0,0 +1,14 @@
+const players = [
+  { name: "CHIBUIKE SUCCESS", price: 10, position: "FWD", injured: false },
+  { name: "OKOYE NELSON", price: 9.5, position: "GK", injured: false },
+  { name: "NNADOZIE DESTINY", price: 10.5, position: "DEF", injured: true },
+  { name: "UKONU ANOINTING", price: 15, position: "MID", injured: false },
+  { name: "IBE EMMANUEL", price: 5, position: "FWD", injured: false },
+  { name: "NWANKWO OSITA", price: 10, position: "DEF", injured: false },
+  { name: "NNONSO KELVIN", price: 15, position: "MID", injured: true },
+  { name: "OKEZIE DAVID", price: 10, position: "DEF", injured: false },
+  { name: "DIKE OGBOGU", price: 18, position: "FWD", injured: false },
+  { name: "OKOYE RICHARD", price: 22, position: "GK", injured: false },
+  { name: "ABAYOMI AYOMIDE", price: 25, position: "MID", injured: false },
+  { name: "OKORONKWO AKACHUKWU", price: 25, position: "FWD", injured: true }
+];
