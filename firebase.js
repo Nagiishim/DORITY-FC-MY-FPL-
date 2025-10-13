3262099b9be36a64e54e2b0b3f602b668c24693b
Nagiishim <Dikeogbogu@gmail.com>
2025-10-13 01:18:01 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,8 @@
-// firebase.js
+// Import Firebase modules
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
 
-// Your Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyCFknCEG0zQjTVpPCs49zm_ERzalXC63Pg",
   authDomain: "dority-fc-site.firebaseapp.com",
@@ -15,8 +13,6 @@ const firebaseConfig = {
   measurementId: "G-FG54ZJ5BRJ"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
